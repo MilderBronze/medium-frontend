b96d644dbdc8836fcc080a01ab5b7c166b0e56d3
MilderBronze <sumitinbki@gmail.com>
2025-12-28 14:23:50 +0530

    added frontend

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Auth } from "../components/Auth";
+import { Quote } from "../components/Quote";
 
-function Signup() {
+export const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
-
-export default Signup
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        {/* its like ki default i need it to be taking one column (when the screen is not large) and i want the screen to be divided into 2 columns when the screen is large */}
+        <div>
+          <Auth type="signup" />
+        </div>
+        <div className="hidden lg:block">
+          <Quote />
+        </div>
+      </div>
+    </div>
+  );
+};
